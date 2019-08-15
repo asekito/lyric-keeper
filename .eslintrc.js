e7c4b8b48ce5fd5e56b4ccac5714f04b72c82e7f
Bruce <bruce@yogainternational.com>
2019-08-14 23:31:43 -0400

    adds api call functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
+      sourceType: module,
     },
     ecmaVersion: 2018,
   },
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   WelcomeText,
   DefaultPageWrapper,
   MainAreaWrapper,
 } from './elements.jsx';
 
-export const App = () => (
-  <DefaultPageWrapper>
-    <WelcomeText>Lyric Keeper</WelcomeText>
-    <MainAreaWrapper>You haven't stored any lyrics, yet</MainAreaWrapper>
-  </DefaultPageWrapper>
-);
+export const App = () => {
+  let lyricData = [];
+
+  useEffect(() => {
+    console.log('Make API call');
+  });
+
+  return (
+    <DefaultPageWrapper>
+      <WelcomeText>Lyric Keeper</WelcomeText>
+      <MainAreaWrapper>
+        {lyricData.length
+          ? lyricData.map(item => item)
+          : "You haven't stored any lyrics, yet"}
+      </MainAreaWrapper>
+    </DefaultPageWrapper>
+  );
+};

796b2ca693136ccf711db5a1880b1ddee9d7075f
Bruce <bruce@yogainternational.com>
2019-08-18 09:07:05 -0400

    updates .gitignore and sets font-sizes to be responsive

--- src/NewLyricModal/elements.jsx
+++ src/NewLyricModal/elements.jsx
@@ -28,12 +28,7 @@ export const HeadingTitle = styled.div`
   font-weight: 700;
   text-align: left;
   color: white;
-<<<<<<< HEAD
-  font-size: 2.8vh;
-  padding: 1.1vw;
-=======
   font-size: 27px;
   padding: 14px;
->>>>>>> 9972962c5981b46c10dff1c31010b21d25c19667
   letter-spacing: 2px;
 `;

a7c3a2415fa3218ac526237fa542166e2f6ba786
Bruce <bruce@yogainternational.com>
2019-08-18 09:02:56 -0400

    merge master into branch

--- src/NewLyricModal/elements.jsx
+++ src/NewLyricModal/elements.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { MainGren } from '../ColorVars.js';
+import { MainGreen } from '../ColorVars.js';
 
 export const ModalContentWrapper = styled.div`
   text-align: center;
@@ -8,8 +8,28 @@ export const ModalContentWrapper = styled.div`
   min-height: 20vh;
   position: absolute;
   margin: auto;
-  border: solid ${MainGren} 3px;
+  border: solid ${MainGreen} 3px;
   border-radius: 6px;
-  top: 34%;
-  left: 34%;
+  top: 16%;
+  left: 20%;
+  overflow: scroll;
+`;
+
+export const TextFieldStyles = styled.div`
+  display: block;
+`;
+
+export const HeadingWrapper = styled.div`
+  width: 100%;
+  min-height: 30px;
+  background-color: ${MainGreen};
+`;
+
+export const HeadingTitle = styled.div`
+  font-weight: 700;
+  text-align: left;
+  color: white;
+  font-size: 2.8vh;
+  padding: 1.1vw;
+  letter-spacing: 2px;
 `;

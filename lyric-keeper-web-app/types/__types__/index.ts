41665b612408cc3504ff404a11f3b8daf99aed44
Bruce Johnson <bruce@yogainternational.com>
2020-06-16 15:36:03 -0400

    adds final codegen improvements

---
+++ lyric-keeper-web-app/types/__types__/index.ts
@@ -0,0 +1,1 @@
+export * from './Query_Get_All_Lyrics'
---
+++ lyric-keeper-web-app/types/generationScript.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+fs.readdir("./__types__", (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const typeFiles = [];
+
+  // Loop over every file in __types__ and add their names to typeFiles
+  files.forEach(file => {
+    if (file) {
+      const fileName = file.split(".ts")[0];
+      if (fileName !== "globalTypes" && fileName !== "index") {
+        typeFiles.push(fileName);
+      }
+    }
+  });
+
+  // Construct index.ts file with all required exports
+  let finalString = "";
+  typeFiles.forEach(name => {
+    console.log(`Adding type for ${name}`);
+    finalString += `export * from './${name}'\n`;
+  });
+  fs.writeFile("__types__/index.ts", finalString, err => {
+    if (err) throw err;
+  });
+
+  // Create final index.ts file to export everything from __types__
+  fs.writeFile("index.ts", 'export * from "./__types__";', err => {
+    if (err) throw err;
+  });
+});

9f3338da26f1b8bc82847ecadd0ba7bb18631538
Bruce Johnson <bruce@yogainternational.com>
2020-06-17 17:39:20 -0400

    adds more console logs for debugging purposes

--- lyric-keeper-web-app/src/LyricPage/LyricView.tsx
+++ lyric-keeper-web-app/src/LyricPage/LyricView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Songtitle, SongAuthor, SongVerse, SongChorus } from "./elements";
 import { Lyric_Without_Short_Url } from "Types";
 
@@ -16,22 +16,33 @@ export const LyricView: React.FC<Lyric_Without_Short_Url> = ({
   author,
   verses,
   chorus,
-}) => (
-  <>
-    <Songtitle>{title}</Songtitle>
-    <SongAuthor>{author}</SongAuthor>
-    {verses && chorus && (
-      <>
-        {verses
-          .split("(chorus)")
-          .map((item: React.ReactNode, index: number, arr: string | any[]) => {
-            if (index < arr.length - 1) {
-              return <ChorusAndVerse chorus={chorus} verse={item} />;
-            } else {
-              return <SongVerse>{item}</SongVerse>;
-            }
-          })}
-      </>
-    )}
-  </>
-);
+}) => {
+  useEffect(() => {
+    console.log(
+      title && author && verses && chorus
+        ? "LyricView mounted with data!"
+        : "LyricView mounted without data!"
+    );
+  }, []);
+  return (
+    <>
+      <Songtitle>{title}</Songtitle>
+      <SongAuthor>{author}</SongAuthor>
+      {verses && chorus && (
+        <>
+          {verses
+            .split("(chorus)")
+            .map(
+              (item: React.ReactNode, index: number, arr: string | any[]) => {
+                if (index < arr.length - 1) {
+                  return <ChorusAndVerse chorus={chorus} verse={item} />;
+                } else {
+                  return <SongVerse>{item}</SongVerse>;
+                }
+              }
+            )}
+        </>
+      )}
+    </>
+  );
+};

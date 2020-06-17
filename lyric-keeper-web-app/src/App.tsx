5feacc9be0b6cb03fe66d40360586ab0c5787339
Bruce Johnson <bruce@yogainternational.com>
2020-06-17 13:18:39 -0400

    adds potential build fixes

--- lyric-keeper-web-app/src/App.tsx
+++ lyric-keeper-web-app/src/App.tsx
@@ -7,18 +7,20 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const client = new ApolloClient({
-  // uri: "https://lyric-keeper-server.herokuapp.com/",
-  uri: "http://localhost:8181/",
+  uri: "https://lyric-keeper-server.herokuapp.com/",
+  // uri: "http://localhost:8181/",
   cache: new InMemoryCache(),
 });
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <Router forceRefresh>
-      <Route path="/" exact component={Homepage} />
-      <Route path="/lyric/:lyricUrl" component={LyricPage} />
-    </Router>
-  </ApolloProvider>
-);
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router forceRefresh>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/lyric/:lyricUrl" component={LyricPage} />
+      </Router>
+    </ApolloProvider>
+  );
+}
 
 export default App;

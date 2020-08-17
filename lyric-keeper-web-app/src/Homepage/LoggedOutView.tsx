b01f2041056fe648e19a045d722cddd457b4f2d6
Bruce Johnson <bruce@yogainternational.com>
2020-08-17 16:31:47 -0400

    updates language and lirbrary access on the logged out homepage

--- lyric-keeper-web-app/src/Homepage/LoggedOutView.tsx
+++ lyric-keeper-web-app/src/Homepage/LoggedOutView.tsx
@@ -11,52 +11,67 @@ import { SecondaryColor, MainGreen } from "ColorVars";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import RightArrow from "@material-ui/icons/ArrowRight";
+import { UseResponsiveCheck } from "Hooks";
 
-export const LoggedOutView: React.FC = () => (
-  <Container maxWidth="md">
-    <PageHeader>Welcome to Lyric Keeper!</PageHeader>
-    <LoggedOutInfoSectionWrapper>
-      <LoggedOutDescriptiveText>
-        <Grid container>
-          <Grid item xs={12} md={1}>
-            <MoneyIcon
-              style={{
-                fontSize: "4rem",
-                color: SecondaryColor,
-              }}
-            />
+export const LoggedOutView: React.FC = () => {
+  const { isMobile } = UseResponsiveCheck();
+  return (
+    <Container maxWidth="md">
+      <PageHeader>Welcome to Lyric Keeper!</PageHeader>
+      <LoggedOutInfoSectionWrapper>
+        <LoggedOutDescriptiveText>
+          <Grid container>
+            <Grid item xs={12} md={1}>
+              <MoneyIcon
+                style={{
+                  fontSize: "4rem",
+                  color: SecondaryColor,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={11}>
+              Are you looking for a simple yet highly effective way to store all
+              your lyrics for FREE?
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={11}>
-            Are you looking for a simple yet highly effective way to store all
-            your lyrics for FREE?
+        </LoggedOutDescriptiveText>
+        <LoggedOutDescriptiveText>
+          <Grid container>
+            <Grid item xs={12} md={1}>
+              <ListIcon
+                style={{
+                  fontSize: "4rem",
+                  color: SecondaryColor,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={11}>
+              Lyric Keeper offers you complete flexibility in the way that you
+              store your lyrics. You can even create collections of lyrics to
+              help keep things neat and orginized!
+            </Grid>
           </Grid>
-        </Grid>
-      </LoggedOutDescriptiveText>
-      <LoggedOutDescriptiveText>
-        <Grid container>
-          <Grid item xs={12} md={1}>
-            <ListIcon
-              style={{
-                fontSize: "4rem",
-                color: SecondaryColor,
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={11}>
-            Lyric Keeper offers you complete flexibility in the way that you
-            store your lyrics. You can even create collections of lyrics to help
-            keep things neat and orginized!
-          </Grid>
-        </Grid>
-      </LoggedOutDescriptiveText>
-    </LoggedOutInfoSectionWrapper>
-    <Link to="/help">
-      <Button
-        variant="outlined"
-        style={{ color: MainGreen, marginTop: "40px", marginBottom: "40px" }}
-      >
-        More information <RightArrow />
-      </Button>
-    </Link>
-  </Container>
-);
+        </LoggedOutDescriptiveText>
+      </LoggedOutInfoSectionWrapper>
+      <div style={{ marginBottom: "40px" }}>
+        <Link to="/help">
+          <Button
+            variant="outlined"
+            style={{ color: MainGreen, marginTop: "40px" }}
+          >
+            More information <RightArrow />
+          </Button>
+        </Link>
+        <Link to="/library" style={{ marginLeft: isMobile ? "" : "20px" }}>
+          <Button
+            variant="outlined"
+            style={{ color: MainGreen, marginTop: "40px" }}
+          >
+            Go to our free lyric library
+            <RightArrow />
+          </Button>
+        </Link>
+      </div>
+    </Container>
+  );
+};
